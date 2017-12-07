424133262fa8478d1e22f87e60a66b455d538e59
bearics <bearics@gmail.com>
2017-12-07 21:37:51 +0900

    fin homework

--- Project1/Project1/graph.h
+++ Project1/Project1/graph.h
@@ -1,8 +1,11 @@
 #pragma once
 
 #include <iostream>
-#include <cstddef>
+#include <cstdlib>
+#include <cassert>
 #include <set>
+#include <limits>
+#include <vector>
 
 using namespace std;
 
@@ -14,25 +17,26 @@ public:
 
 	// constructor
 	graph() : many_vertices(0) {}
+	void add_vertex(const Item& label);
+	void add_edge(size_t source, size_t target, Item data);
+	void remove_edge(size_t source, size_t target);
+	Item& operator[](size_t vertex);
+	size_t size() { return many_vertices; }
+	Item is_edge(size_t source, size_t target);
+	set<size_t> neighbors(size_t vertex);
+	void shortestPath(size_t start);
 
 private:
 	Item edges[MAX][MAX];
 	Item labels[MAX];
 	size_t many_vertices;
 
-	void add_vertex(const Item& label);
-	void add_edge(size_t source, size_t target, Item data);
-	void remove_edge(size_t source, size_t target);
-	Item& operator[](size_t vertex);
-	size_t size() { return many_vertices;   }
-	Item is_edge(size_t source, size_t target) const;
-	set<size_t> neighbors(size_t vertex) const;
-	void shortestPath(size_t start);
+
 
 };
 /*
 	function that finds and prints all the shortest paths and
-	distances from the start vertex to every other vertex 
+	distances from the start vertex to every other vertex
 	in the graph using Dijkstra¡¯s algorithm.
 
 	The output should look like the following
@@ -47,7 +51,92 @@ private:
 template<class Item>
 void graph<Item>::shortestPath(size_t start)
 {
+	set<size_t> allowed_vertices;
+	set<size_t> neighbor;
 
+	set<size_t>::iterator vit;
+	Item distance[MAX];
+	vector<size_t> path[MAX];
+	size_t now = start;
+	size_t next = start;
+
+
+	assert(start < size());
+
+	// setting all label to infinity
+	for (int i = 0; i < many_vertices; i++)
+	{
+		distance[i] = numeric_limits<Item>::infinity();
+		path[i].push_back(start);
+	}
+	distance[start] = 0;
+	allowed_vertices.insert(start);
+
+	for (int i = 0; i < (many_vertices-1); i++)
+	{
+
+		neighbor = neighbors(now);
+		Item temp = 0;
+		Item min = numeric_limits<Item>::infinity();
+
+		if ( !neighbor.empty() )
+		{
+			set<size_t>::iterator it = neighbor.begin();
+
+			do
+			{
+				temp = is_edge(now, *it);
+
+
+				if (distance[*it] > (distance[now] + temp))
+				{
+					distance[*it] = (distance[now] + temp);
+					path[*it] = path[now];
+					path[*it].push_back(*it);
+				}
+
+
+			} while (++it != neighbor.end());
+
+		}
+
+
+		for (int j = 0; j < many_vertices; j++)
+		{
+			if (std::find(allowed_vertices.begin(), allowed_vertices.end(), j) != allowed_vertices.end())
+				continue;
+			if (min > distance[j])
+			{
+				min = distance[j];
+				now = j;
+			}
+		}
+
+		allowed_vertices.insert(now);
+
+
+	}
+
+	cout << "From vertex " << start << "," << endl;
+	for (int i = 0; i < many_vertices ; i++)
+	{
+		vector<size_t>::iterator it;
+		cout << "the shortest path to vertex " << i << " is ";
+		for (it = path[i].begin(); it != path[i].end(); it++)
+		{
+			if (path[i].size() == 1)
+			{
+				cout << "EMPTY";
+				continue;
+			}
+			if (it != path[i].begin())
+				cout << ", " << *it;
+			else
+				cout << *it;
+		}
+		cout << " with distance " << distance[i] << ".\n";
+	}
+	
 }
 
 template<class Item>
@@ -74,7 +163,7 @@ void graph<Item>::add_vertex(const Item& label)
 }
 
 template<class Item>
-void graph<Item>::add_edge(size_t source, size_t target, Item data )
+void graph<Item>::add_edge(size_t source, size_t target, Item data)
 {
 	assert(source < size());
 	assert(target < size());
@@ -82,7 +171,7 @@ void graph<Item>::add_edge(size_t source, size_t target, Item data )
 }
 
 template<class Item>
-Item graph<Item>::is_edge(size_t source, size_t target) const
+Item graph<Item>::is_edge(size_t source, size_t target)
 {
 	assert(source < size());
 	assert(target < size());
@@ -90,7 +179,7 @@ Item graph<Item>::is_edge(size_t source, size_t target) const
 }
 
 template <class Item>
-set<size_t> graph<Item>::neighbors(size_t vertex) const
+set<size_t> graph<Item>::neighbors(size_t vertex)
 {
 	set<size_t> answer;
 	size_t i;
--- Project1/Project1/main.cpp
+++ Project1/Project1/main.cpp
@@ -10,8 +10,44 @@ using namespace std;
 
 int main(void)
 {
+	graph<double> g;
 
-	while(1){}
+	// Add vertex 6
+	for (int i = 0; i < 10; i++)
+	{
+		g.add_vertex((double)i);
+	}
+
+	g.add_edge(0, 1, 2);
+	g.add_edge(0, 7, 8);
+	g.add_edge(1, 8, 9);
+	g.add_edge(1, 9, 6);
+	g.add_edge(6, 7, 15);
+	g.add_edge(9, 6, 1);
+	g.add_edge(8, 6, 7);
+	g.add_edge(8, 2, 3);
+	g.add_edge(6, 2, 12);
+	g.add_edge(9, 2, 1);
+	g.add_edge(5, 9, 9);
+	g.add_edge(4, 5, 11);
+	g.add_edge(3, 4, 3);
+	g.add_edge(2, 3, 8);
+	g.add_edge(8, 3, 1);
+
+	for (int i = 0; i < 10; i++)
+	{
+		for (int j = 0; j < 10; j++)
+		{
+			cout.width(5);
+			cout << g.is_edge(i, j);
+		}
+		cout << endl;
+	}
+
+	g.shortestPath(0);
+
+	g.shortestPath(5);
+	while (1) {}
 
 	return 0;
 }
